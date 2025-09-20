5a5243cfd10a5a401c935f35decf572dfd3f050c
alchab54 <alicechabaux@gmail.com>
2025-09-21 00:35:31 +0200

    Mes changements avant la mise à jour

--- server_v4_complete.py
+++ server_v4_complete.py
@@ -91,7 +91,15 @@ def create_app(config=None):
     def health_check():
         """Route simple pour le healthcheck de Docker."""
         return jsonify({"status": "healthy"}), 200
+
+    
+
+    @app.route("/api/health", methods=["GET"])
+    def health_check():
+        """Route simple pour le healthcheck de Docker."""
+        return jsonify({"status": "healthy"}), 200
     # -------------------------
+
 
     # --- NOUVEAUX ENDPOINTS POUR L'EXTENSION ZOTERO ---
 
