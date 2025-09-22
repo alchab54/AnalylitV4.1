dc966b639d19604a8c18d4d165f7a61c46b76d80
alchab54 <alicechabaux@gmail.com>
2025-09-22 13:07:33 +0200

    Mes changements avant la mise à jour

--- database/init-user-db.sql
+++ database/init-user-db.sql
@@ -1,12 +1,23 @@
+-- Vérifier si la base existe avant de créer (PostgreSQL ne supporte pas IF NOT EXISTS dans CREATE DATABASE)
 DO
 $do$
 BEGIN
-   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'analylit_user') THEN
-      CREATE ROLE analylit_user WITH LOGIN PASSWORD 'strong_password';
+   IF NOT EXISTS (
+      SELECT FROM pg_database WHERE datname = 'analylit_db'
+   ) THEN
+      PERFORM dblink_exec('dbname=postgres', 'CREATE DATABASE analylit_db OWNER analylit_user');
    END IF;
 END
 $do$;
 
-ALTER ROLE analylit_user CREATEDB;
-
-CREATE DATABASE analylit_db OWNER analylit_user IF NOT EXISTS;
+-- Création du rôle utilisateur (adapté)
+DO
+$do$
+BEGIN
+   IF NOT EXISTS (
+      SELECT FROM pg_roles WHERE rolname = 'analylit_user'
+   ) THEN
+      CREATE ROLE analylit_user WITH LOGIN PASSWORD 'strong_password';
+   END IF;
+END
+$do$;
