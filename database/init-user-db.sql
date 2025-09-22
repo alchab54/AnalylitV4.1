e35497c9a23c5e49a9b4a75b86914ed19132e3f4
alchab54 <alicechabaux@gmail.com>
2025-09-22 13:01:56 +0200

    Mes changements avant la mise à jour

--- database/init-user-db.sql
+++ database/init-user-db.sql
@@ -1,3 +1,12 @@
-CREATE ROLE analylit_user WITH LOGIN PASSWORD 'strong_password';
+DO
+$do$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'analylit_user') THEN
+      CREATE ROLE analylit_user WITH LOGIN PASSWORD 'strong_password';
+   END IF;
+END
+$do$;
+
 ALTER ROLE analylit_user CREATEDB;
-CREATE DATABASE analylit_db OWNER analylit_user;
+
+CREATE DATABASE analylit_db OWNER analylit_user IF NOT EXISTS;
