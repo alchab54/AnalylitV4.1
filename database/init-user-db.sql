7e9866716d4eb069e8b7e0a454df040af7af9819
alchab54 <alicechabaux@gmail.com>
2025-09-22 13:01:02 +0200

    Mes changements avant la mise à jour

--- database/init-user-db.sql
+++ database/init-user-db.sql
@@ -1,1 +1,3 @@
-CREATE DATABASE analylit_user;
+CREATE ROLE analylit_user WITH LOGIN PASSWORD 'strong_password';
+ALTER ROLE analylit_user CREATEDB;
+CREATE DATABASE analylit_db OWNER analylit_user;
