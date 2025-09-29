28fe6fcb283bf769d9cab80162297aa67a8702c1
alchab54 <alicechabaux@gmail.com>
2025-09-29 23:28:13 +0200

    Mes changements

--- migrations/env.py
+++ migrations/env.py
@@ -88,7 +88,7 @@ def run_migrations_online():
     and associate a connection with the context.
 
     """
-
+    logger.info("Running migrations in online mode...")
     connectable = engine_from_config(
         config.get_section(config.config_ini_section),
         prefix="sqlalchemy.",
